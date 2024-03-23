3098dbc9caedf3f798a51ef38587fd030fe53ac5
+ <+>
2024-03-23 17:04:11 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${"Guadalupe" ,"Ollie" , "Aki"[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // countDown function
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+
+  
